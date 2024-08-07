550a92702448e6dad178c0c075369828d6aa3639
malkaviano <rafael.kss@gmail.com>
2024-08-06 21:18:30 -0300

    remove anim data asset

--- Source/AGDemonstration/Private/Animation/AGD_AnimInstance.cpp
+++ Source/AGDemonstration/Private/Animation/AGD_AnimInstance.cpp
@@ -1,35 +1,3 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Animation/AGD_AnimInstance.h"
-#include "Animation/AnimationAsset.h"
-
-UBlendSpace* UAGD_AnimInstance::GetUnarmedBlendSpace() const
-{
-    return CharacterAnimDataAsset->CharacterAnimationData.UnarmedData
-        .BlendSpace;
-}
-
-UAnimSequenceBase*
-UAGD_AnimInstance::GetUnarmedIdleAnimation(const int32 Index) const
-{
-    return CharacterAnimDataAsset->CharacterAnimationData.UnarmedData
-        .IdleAnimationAssets[Index];
-}
-
-UBlendSpace* UAGD_AnimInstance::GetUnarmedCrouchBlendSpace() const
-{
-    return CharacterAnimDataAsset->CharacterAnimationData.UnarmedData
-        .CrouchedBlendSpace;
-}
-
-UAnimSequenceBase*
-UAGD_AnimInstance::GetUnarmedCrouchIdleAnimation(const int32 Index) const
-{
-    return CharacterAnimDataAsset->CharacterAnimationData.UnarmedData
-        .CrouchedIdleAnimationAssets[Index];
-}
-
-UBlendSpace* UAGD_AnimInstance::GetUnarmedSprintBlendSpace() const {
-     return CharacterAnimDataAsset->CharacterAnimationData.UnarmedData
-        .SprintBlendSpace;
-}
--- Source/AGDemonstration/Public/Animation/AGD_AnimInstance.h
+++ Source/AGDemonstration/Public/Animation/AGD_AnimInstance.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
-#include "Data/Asset/AGD_CharacterAnimDataAsset.h"
 #include "UObject/ObjectMacros.h"
 
 #include "AGD_AnimInstance.generated.h"
@@ -17,21 +16,4 @@ class AGDEMONSTRATION_API UAGD_AnimInstance : public UAnimInstance {
     GENERATED_BODY()
 
   protected:
-    UPROPERTY(EditDefaultsOnly, Category = "Data Asset")
-    TObjectPtr<UAGD_CharacterAnimDataAsset> CharacterAnimDataAsset;
-
-    UFUNCTION(BlueprintCallable, meta = (BlueprintThreadSafe))
-    virtual UBlendSpace* GetUnarmedBlendSpace() const;
-
-    UFUNCTION(BlueprintCallable, meta = (BlueprintThreadSafe))
-    virtual UAnimSequenceBase* GetUnarmedIdleAnimation(const int32 Index = 0) const;
-
-    UFUNCTION(BlueprintCallable, meta = (BlueprintThreadSafe))
-    virtual UBlendSpace* GetUnarmedCrouchBlendSpace() const;
-
-    UFUNCTION(BlueprintCallable, meta = (BlueprintThreadSafe))
-    virtual UAnimSequenceBase* GetUnarmedCrouchIdleAnimation(const int32 Index = 0) const;
-
-    UFUNCTION(BlueprintCallable, meta = (BlueprintThreadSafe))
-    virtual UBlendSpace* GetUnarmedSprintBlendSpace() const;
 };
